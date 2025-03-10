f6685ff17dbafcfb889f3dfb046b3f89c8bad2a9
kaeun0813 <rk081321@gmail.com>
2025-03-10 23:23:08 +0900

    fix: deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,12 +8,23 @@ echo "Stopping the current Flask application..."
 
 #  최신 코드 가져오기
 echo "Pulling the latest code from Git..."
-git pull origin main  # main 브랜치에서 최신 코드 가져오기
 
 #  가상환경 활성화 및 패키지 설치 (필요할 경우)
 echo "Installing dependencies..."
+
+#배포된 디렉터리로 이동
+cd /home/ubuntu/Howkiki_AI
+
+# 가상환경이 없으면 생성
+if [ ! -d "venv" ]; then
+  echo "Virtual environment not found! Creating venv..."
+  python3 -m venv venv
+fi
+
+# 가상환경 활성화 및 패키지 설치
+echo "Activating virtual environment..."
 source venv/bin/activate  # 가상환경 활성화
-#pip install -r requirements.txt  # 필요한 패키지 설치
+pip install -r requirements.txt  # 필요한 패키지 설치
 
 #  Flask 애플리케이션 재시작
 echo "Restarting the Flask application..."
