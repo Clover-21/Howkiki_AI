f2094f13b5dcb98d758306670671cde19f690ba0
kaeun0813 <rk081321@gmail.com>
2025-03-11 16:18:07 +0900

    fix: 포트 수정

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ app.register_blueprint(chatbot_bp, url_prefix='/api')
 
 # 서버 실행
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
     
